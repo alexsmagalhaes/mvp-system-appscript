28165df81eaa5d022e953d25a2c754bafe52e164
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-03-17 11:43:22 -0300

    fix: adjust throw message

--- src/server/utils/error.util.js
+++ src/server/utils/error.util.js
@@ -1,15 +1,14 @@
 const ErrorHandler = {
-    send(status, errorMessage, payload) {
-      const error = {
-        status: Number.parseInt(status),
-        errors: [errorMessage],
-      };
-  
-      if (payload) {
-        Object.assign(error, payload);
-      }
-  
-      throw new Error(JSON.stringify(error));
-    },
-  };
-  
+  send(status, errorMessage, payload) {
+    const error = {
+      status: Number.parseInt(status),
+      errors: [errorMessage],
+    };
+
+    if (payload) {
+      Object.assign(error, payload);
+    }
+
+    throw JSON.stringify(error);
+  },
+};
