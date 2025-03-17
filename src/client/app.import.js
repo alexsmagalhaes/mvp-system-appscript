b6d95002a4c61792bbe3bf8d37f4cb6093f45a5a
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2025-03-17 11:47:57 -0300

    feat: add initial font-end structure

---
+++ src/client/app.import.js
@@ -0,0 +1,12 @@
+const ImportManager = {
+  base: "src",
+  importList: [
+    // "src/client/utils/session-storage",
+    "src/client/hooks/useAuth",
+    "src/client/hooks/useCookies",
+    "src/client/hooks/useGoogleScript",
+    "src/client/pages/dashboard",
+    "src/client/pages/login",
+    "src/client/app.component",
+  ],
+};
